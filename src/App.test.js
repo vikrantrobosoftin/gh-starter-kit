6dd515bdccde33dc0f5394aeb4b1eb9078f2f0f7
vikrantrobosoftin <vikrant.shroti@robosoftin.com>
2020-12-21 17:03:45 +0530

    Adding Button and InputText components and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import Button from "../src/components/Button";
+import InputText from "../src/components/InputText";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it("capture clicks", (done) => {
+  function handleClick() {
+    done();
+  }
+  const { getByText } = render(<Button onClick={handleClick}>Click me</Button>);
+  const node = getByText("Click me");
+  fireEvent.click(node);
+});
+
+it("capture changes", (done) => {
+  function handleChange(evt) {
+    expect(evt.target.value).toEqual("whamo");
+    done();
+  }
+
+  const { getByPlaceholderText } = render(
+    <InputText onChange={handleChange} placeholder="Change me" />
+  );
+
+  const node = getByPlaceholderText("Change me");
+  fireEvent.change(node, { target: { value: "whamo" } });
+});
+
+afterEach(cleanup);
